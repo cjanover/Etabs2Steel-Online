8a178b80210c250942b583d643bd5175326793ed
cjanover <chrisjanover@gmail.com>
2015-01-30 10:54:33 -0800

    removed publisher

--- app/services/publisher.rb
+++
@@ -1,8 +0,0 @@
-class Publisher
-  # In order to publish message we need a exchange name.
-  # Note that RabbitMQ does not care about the payload -
-  # we will be using JSON-encoded strings
-  def self.publish(exchange, message = {})
-
-  end
-end
